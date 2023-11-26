edca14c8b0b48f62bb3b7f71a0a21000509769bf
Mohess <me594472@gmail.com>
2023-11-26 21:59:30 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// let car = ""
+// for(let i = 1990 ; i<=2023; i++) {
+//      car = car + '<option>' + i + '</option>'
+
+// }
+
+// document.getElementById("demo").innerHTML=car  
+
+//  PR"ERWE"?{_
+
+
+function getSum (x , y) {
+     console.log( x)
+}
+
+getSum(10 , 10)
+getSum(10 , 30)
+getSum(10 , 50)
+getSum(10 , 60)
+getSum(10 , 70)
+getSum(10 , 90)
